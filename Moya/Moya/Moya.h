297d396a97639607704b256cf2fb56832b2da73a
Ash Furrow <ash@ashfurrow.com>
2014-08-16 10:00:16 +0200

    Added basic project and tests.

---
+++ Moya/Moya/Moya.h
@@ -0,0 +1,19 @@
+//
+//  Moya.h
+//  Moya
+//
+//  Created by Ash Furrow on 2014-08-16.
+//  Copyright (c) 2014 Ash Furrow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Moya.
+FOUNDATION_EXPORT double MoyaVersionNumber;
+
+//! Project version string for Moya.
+FOUNDATION_EXPORT const unsigned char MoyaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Moya/PublicHeader.h>
+
+
