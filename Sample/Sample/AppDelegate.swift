c40f4b00310d1bed8cac0b1d3abb63f732cbd9ed
Ash Furrow <ash@ashfurrow.com>
2014-09-07 06:42:30 +0200

    Sample app.

---
+++ Sample/Sample/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Sample
+//
+//  Created by Ash Furrow on 2014-09-07.
+//  Copyright (c) 2014 Ash Furrow. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+                            
+    var window: UIWindow?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
