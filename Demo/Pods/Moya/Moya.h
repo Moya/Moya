1d6d0801812cace699ea1f87324728dab0b34085
Justin Makaila <justinmakaila@gmail.com>
2015-06-25 22:21:57 -0400

    You can tell I'm partial to Carthage.

---
+++ Demo/Pods/Moya/Moya.h
@@ -0,0 +1,19 @@
+//
+//  Moya.h
+//  Moya
+//
+//  Created by Justin Makaila on 6/25/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Moya.
+FOUNDATION_EXPORT double MoyaVersionNumber;
+
+//! Project version string for Moya.
+FOUNDATION_EXPORT const unsigned char MoyaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Moya/PublicHeader.h>
+
+
