63092d3c2fe04bbff348368f2e3e227dcc302938
Justin Makaila <justinmakaila@gmail.com>
2015-07-14 18:12:06 -0400

    Added Carthage build dir for easy demo execution

---
+++ Carthage/Build/Mac/Box.framework/Versions/A/Headers/Box.h
@@ -0,0 +1,7 @@
+//  Copyright (c) 2014 Rob Rix. All rights reserved.
+
+/// Project version number for Box.
+extern double BoxVersionNumber;
+
+/// Project version string for Box.
+extern const unsigned char BoxVersionString[];
