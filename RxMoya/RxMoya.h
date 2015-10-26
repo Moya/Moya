9ee73026ba8e2bf4394de7d795ba42b778b3fea7
John Heaton <jheaton.dev@gmail.com>
2015-10-26 13:45:17 -0400

    Project w/ shared iOS framework targets for Carthage

---
+++ RxMoya/RxMoya.h
@@ -0,0 +1,19 @@
+//
+//  RxMoya.h
+//  RxMoya
+//
+//  Created by John Heaton on 10/26/15.
+//  Copyright © 2015 StageBloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RxMoya.
+FOUNDATION_EXPORT double RxMoyaVersionNumber;
+
+//! Project version string for RxMoya.
+FOUNDATION_EXPORT const unsigned char RxMoyaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxMoya/PublicHeader.h>
+
+
