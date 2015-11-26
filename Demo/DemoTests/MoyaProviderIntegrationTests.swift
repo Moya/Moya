55745d99fc846b091c4cc643d54b65f6d030ccf6
Pierre-Marc Airoldi <pierremarcairoldi@gmail.com>
2015-11-26 01:18:50 -0500

    update network logging plugin
    Changes:
    
    - Updates network logging plugin to make more testable by using
    dependency injection for print
    - Add tests for network logging plugin

--- Demo/DemoTests/MoyaProviderIntegrationTests.swift
+++ Demo/DemoTests/MoyaProviderIntegrationTests.swift
@@ -19,12 +19,12 @@ class MoyaProviderIntegrationTests: QuickSpec {
     override func spec() {
         let userMessage = NSString(data: GitHub.UserProfile("ashfurrow").sampleData, encoding: NSUTF8StringEncoding)
         let zenMessage = NSString(data: GitHub.Zen.sampleData, encoding: NSUTF8StringEncoding)
-
+        
         beforeEach {
             OHHTTPStubs.stubRequestsPassingTest({$0.URL!.path == "/zen"}) { _ in
                 return OHHTTPStubsResponse(data: GitHub.Zen.sampleData, statusCode: 200, headers: nil).responseTime(0.5)
             }
-
+            
             OHHTTPStubs.stubRequestsPassingTest({$0.URL!.path == "/users/ashfurrow"}) { _ in
                 return OHHTTPStubsResponse(data: GitHub.UserProfile("ashfurrow").sampleData, statusCode: 200, headers: nil).responseTime(0.5)
             }
@@ -34,11 +34,11 @@ class MoyaProviderIntegrationTests: QuickSpec {
             }
             
         }
-
+        
         afterEach {
             OHHTTPStubs.removeAllStubs()
         }
-
+        
         describe("valid endpoints") {
             describe("with live data") {
                 describe("a provider") {
@@ -76,7 +76,7 @@ class MoyaProviderIntegrationTests: QuickSpec {
                     
                     it("returns an error when cancelled") {
                         var receivedError: ErrorType?
-
+                        
                         let target: GitHub = .UserProfile("ashfurrow")
                         let token = provider.request(target) { result in
                             if case let .Failure(error) = result {
@@ -84,7 +84,7 @@ class MoyaProviderIntegrationTests: QuickSpec {
                             }
                         }
                         token.cancel()
-
+                        
                         expect(receivedError).toEventuallyNot( beNil() )
                     }
                 }
@@ -127,7 +127,7 @@ class MoyaProviderIntegrationTests: QuickSpec {
                         expect(returnedData).toEventually(equal(target.sampleData))
                     }
                 }
-
+                
                 describe("a provider with network activity plugin") {
                     it("notifies at the beginning of network requests") {
                         var called = false
@@ -140,10 +140,10 @@ class MoyaProviderIntegrationTests: QuickSpec {
                         let provider = MoyaProvider<GitHub>(plugins: [plugin])
                         let target: GitHub = .Zen
                         provider.request(target) { _ in  }
-
+                        
                         expect(called).toEventually( beTrue() )
                     }
-
+                    
                     it("notifies at the end of network requests") {
                         var called = false
                         let plugin = NetworkActivityPlugin { (change) -> () in
@@ -151,12 +151,36 @@ class MoyaProviderIntegrationTests: QuickSpec {
                                 called = true
                             }
                         }
-
+                        
                         let provider = MoyaProvider<GitHub>(plugins: [plugin])
                         let target: GitHub = .Zen
                         provider.request(target) { _ in  }
-
+                        
                         expect(called).toEventually( beTrue() )
+                    }
+                }
+                
+                describe("a provider with network logger plugin") {
+                    
+                    it("logs the request") {
+                        var log = ""
+                        let plugin = NetworkLoggerPlugin(verbose: true, output: { printing in
+                            //mapping the Any... from items to a string that can be compared
+                            log += printing.items.reduce("") { $0 + ($1 as! [String]).reduce("") { acc, elem in
+                                return acc + elem.stringByReplacingOccurrencesOfString("\n", withString: "").stringByReplacingOccurrencesOfString("    ", withString: "") + " "
+                                }
+                            }
+                        })
+                        
+                        let provider = MoyaProvider<GitHub>(plugins: [plugin])
+                        provider.request(GitHub.Zen) { _ in  }
+                        
+                        expect(log).toEventually(contain("Request:"))
+                        expect(log).toEventually(contain("{ URL: https://api.github.com/zen }"))
+                        expect(log).toEventually(contain("Request Headers: [:]"))
+                        expect(log).toEventually(contain("HTTP Request Method: GET"))
+                        expect(log).toEventually(contain("Response:"))
+                        expect(log).toEventually(contain("{ URL: https://api.github.com/zen } { status code: 200, headers {\"Content-Length\" = 43;} }"))
                     }
                 }
                 
@@ -175,7 +199,7 @@ class MoyaProviderIntegrationTests: QuickSpec {
                                 message = NSString(data: response.data, encoding: NSUTF8StringEncoding) as? String
                             }
                         }
-
+                        
                         expect{message}.toEventually( equal(zenMessage) )
                     }
                     
@@ -188,7 +212,7 @@ class MoyaProviderIntegrationTests: QuickSpec {
                                 message = NSString(data: response.data, encoding: NSUTF8StringEncoding) as? String
                             }
                         }
-
+                        
                         expect{message}.toEventually( equal(userMessage) )
                     }
                 }
