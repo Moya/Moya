a6c125fd52c1594ec74d86b718a34fcdff93aeb3
Pierre-Marc Airoldi <pierremarcairoldi@gmail.com>
2015-11-25 22:24:35 -0500

    move endpoint extensions to the right file
    Changes:
    
    - Move Equtable and Hashable support to Endpoint.swift

--- Source/Response.swift
+++ Source/Response.swift
@@ -73,15 +73,3 @@ public extension Response {
         return string as String
     }
 }
-
-/// Required for making Endpoint conform to Equatable.
-public func ==<T>(lhs: Endpoint<T>, rhs: Endpoint<T>) -> Bool {
-    return lhs.urlRequest.isEqual(rhs.urlRequest)
-}
-
-/// Required for using Endpoint as a key type in a Dictionary.
-extension Endpoint: Equatable, Hashable {
-    public var hashValue: Int {
-        return urlRequest.hash
-    }
-}
