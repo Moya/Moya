6336e866f83b0382676ce0fd3c8d4def3d750e98
Antoine van der Lee <a.vanderlee@triple-it.nl>
2016-07-14 14:43:43 +0200

    Update for Swift 2.3

--- Source/Moya.swift
+++ Source/Moya.swift
@@ -295,7 +295,7 @@ public extension MoyaProvider {
     // These functions are default mappings to MoyaProvider's properties: endpoints, requests, manager, etc.
 
     public final class func DefaultEndpointMapping(target: Target) -> Endpoint<Target> {
-        let url = target.baseURL.URLByAppendingPathComponent(target.path).absoluteString
+        let url = target.baseURL.URLByAppendingPathComponent(target.path)!.absoluteString
         return Endpoint(URL: url, sampleResponseClosure: {.NetworkResponse(200, target.sampleData)}, method: target.method, parameters: target.parameters)
     }
 
