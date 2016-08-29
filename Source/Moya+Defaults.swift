d7058f6fa8521ff44675b1559bbfd8ad2bff86b6
Gustavo Gervasio <gustavogervasio@gmail.com>
2016-08-29 14:01:45 -0300

    add suport to swift 2.3

--- Source/Moya+Defaults.swift
+++ Source/Moya+Defaults.swift
@@ -3,8 +3,17 @@ import Alamofire
 /// These functions are default mappings to `MoyaProvider`'s properties: endpoints, requests, manager, etc.
 public extension MoyaProvider {
     public final class func DefaultEndpointMapping(target: Target) -> Endpoint<Target> {
-        let url = target.baseURL.URLByAppendingPathComponent(target.path).absoluteString
-        return Endpoint(URL: url, sampleResponseClosure: {.NetworkResponse(200, target.sampleData)}, method: target.method, parameters: target.parameters)
+        guard let stringURL = target.baseURL.URLByAppendingPathComponent(target.path)?.absoluteString  else {
+               fatalError("\(target) url is not valid")
+        }
+        return Endpoint(URL: stringURL, sampleResponseClosure: {.NetworkResponse(200, target.sampleData)}, method: target.method, parameters: target.parameters)
+        /*
+        if let url = target.baseURL.URLByAppendingPathComponent(target.path) {
+            if let stringURL = url.absoluteString {
+                return Endpoint(URL: url!, sampleResponseClosure: {.NetworkResponse(200, target.sampleData)}, method: target.method, parameters: target.parameters)
+            }
+        }
+        fatalError("\(target) url is not valid")*/
     }
 
     public final class func DefaultRequestMapping(endpoint: Endpoint<Target>, closure: RequestResultClosure) {
