4d84f631e46eec749f40028e7d12f1a72a4e7666
Fernando Paredes <nano@fdp.io>
2016-09-25 17:47:45 -0700

    [Demo] Remove AnyObject casts in TargetType examples

--- Demo/Demo/GiphyAPI.swift
+++ Demo/Demo/GiphyAPI.swift
@@ -24,7 +24,7 @@ extension Giphy: TargetType {
     public var parameters: [String: Any]? {
         switch self {
         case .upload:
-            return ["api_key": "dc6zaTOxFJmzC" as AnyObject, "username": "Moya" as AnyObject]
+            return ["api_key": "dc6zaTOxFJmzC", "username": "Moya"]
         }
     }
     public var task: Task {
