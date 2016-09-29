f19b930b905dfb56dfa134daed65e08b35e3eb9f
Andrew Breckenridge <asbreckenridge@me.com>
2016-09-29 14:26:51 -0700

    create iOS, macOS, tvOS test targets

---
+++ MoyaTests-macOS/MoyaTests_macOS.swift
@@ -0,0 +1,35 @@
+//
+//  MoyaTests_macOS.swift
+//  MoyaTests-macOS
+//
+//  Created by Andrew Breckenridge on 9/29/16.
+//  Copyright © 2016 StageBloc. All rights reserved.
+//
+
+import XCTest
+
+class MoyaTests_macOS: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
