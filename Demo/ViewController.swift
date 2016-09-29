843d9ddae78a6946974586c11e8e239395e65995
Andrew Breckenridge <asbreckenridge@me.com>
2016-09-29 13:49:33 -0700

    create Demo target in Moya.xcodeproj

---
+++ Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by Andrew Breckenridge on 9/29/16.
+//  Copyright © 2016 StageBloc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
