5e6f465f89ed2fdf3d3bf55cc8e8b55f5db14e92
Łukasz Mróz <lukasz.mroz@droidsonroids.pl>
2016-11-20 04:00:42 +0100

    [MultiTarget] Add MultiTarget target to Demo

---
+++ Demo/DemoMultiTarget/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DemoMultiTarget
+//
+//  Created by Lukasz Mroz on 20.11.2016.
+//  Copyright © 2016 Ash Furrow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
