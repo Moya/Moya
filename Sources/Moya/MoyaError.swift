88f32fd98d545ef44c809d980fef956a75d1474a
Scott Hoyt <scottrhoyt@gmail.com>
2017-01-03 21:45:16 -0800

    Add MoyaError documentation.

--- Sources/Moya/MoyaError.swift
+++ Sources/Moya/MoyaError.swift
@@ -5,11 +5,23 @@ import Foundation
 public typealias Error = MoyaError
 
 public enum MoyaError: Swift.Error {
+
+    /// Indicates a response failed to map to an image.
     case imageMapping(Response)
+
+    /// Indicates a response failed to map to a JSON structure.
     case jsonMapping(Response)
+
+    /// Indicates a response failed to map to a String.
     case stringMapping(Response)
+
+    /// Indicates a response failed with an invalid HTTP status code.
     case statusCode(Response)
+
+    /// Indicates a response failed due to an underlying `Error`.
     case underlying(Swift.Error)
+
+    /// Indicates that an `Endpoint` failed to map to a `URLRequest`.
     case requestMapping(String)
 }
 
