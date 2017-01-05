f443417568bef6398c1b8f9a9bb5ac892ad88a34
Tommy Sadiq Hinrichsen <tommy.lynge@gmail.com>
2017-01-05 14:55:42 +0100

    Update RxMoyaProvider.swift

--- Sources/RxMoya/RxMoyaProvider.swift
+++ Sources/RxMoya/RxMoyaProvider.swift
@@ -22,7 +22,7 @@ open class RxMoyaProvider<Target>: MoyaProvider<Target> where Target: TargetType
         // Creates an observable that starts a request each time it's subscribed to.
         return Observable.create { [weak self] observer in
             guard let strongSelf = self else {
-                observer.onError(RxError.disposed(object: self))
+                observer.onError(RxError.disposed(object: self?))
             }
             let cancellableToken = strongSelf.request(token) { result in
                 switch result {
