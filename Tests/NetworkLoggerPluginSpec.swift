3f9e080cda324fd2e27ce6a9b6798fb03be91ae8
Julien <jgoudet@cityway.fr>
2017-04-04 14:07:48 +0200

    Change URLResponse to HTTPURLResponse
    * Change in project
    * Change in Tests

--- Tests/NetworkLoggerPluginSpec.swift
+++ Tests/NetworkLoggerPluginSpec.swift
@@ -63,7 +63,7 @@ final class NetworkLoggerPluginSpec: QuickSpec {
         }
 
         it("outputs the response data") {
-            let response = Response(statusCode: 200, data: "cool body".data(using: .utf8)!, response: URLResponse(url: URL(string: url(GitHub.zen))!, mimeType: nil, expectedContentLength: 0, textEncodingName: nil))
+            let response = Response(statusCode: 200, data: "cool body".data(using: .utf8)!, response: HTTPURLResponse(url: URL(string: url(GitHub.zen))!, mimeType: nil, expectedContentLength: 0, textEncodingName: nil))
             let result: Result<Moya.Response, MoyaError> = .success(response)
 
             plugin.didReceive(result, target: GitHub.zen)
@@ -74,7 +74,7 @@ final class NetworkLoggerPluginSpec: QuickSpec {
         }
 
         it("outputs the formatted response data") {
-            let response = Response(statusCode: 200, data: "cool body".data(using: .utf8)!, response: URLResponse(url: URL(string: url(GitHub.zen))!, mimeType: nil, expectedContentLength: 0, textEncodingName: nil))
+            let response = Response(statusCode: 200, data: "cool body".data(using: .utf8)!, response: HTTPURLResponse(url: URL(string: url(GitHub.zen))!, mimeType: nil, expectedContentLength: 0, textEncodingName: nil))
             let result: Result<Moya.Response, MoyaError> = .success(response)
 
             pluginWithResponseDataFormatter.didReceive(result, target: GitHub.zen)
