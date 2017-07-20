73e9da97063d161223fe9ea6d29732e85e3db47e
Łukasz Mróz <lukasz.mroz@droidsonroids.pl>
2017-07-20 22:37:40 +0200

    [General] Make URL from target a public init.

---
+++ Sources/Moya/URL+Moya.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+public extension URL {
+
+    /// Initialize URL from Moya's `TargetType`.
+    init<T: TargetType>(target: T) {
+        // When a TargetType's path is empty, URL.appendingPathComponent may introduce trailing /, which may not be wanted in some cases
+        // See: https://github.com/Moya/Moya/pull/1053
+        // And: https://github.com/Moya/Moya/issues/1049
+        if target.path.isEmpty {
+            self = target.baseURL
+        } else {
+            self = target.baseURL.appendingPathComponent(target.path)
+        }
+    }
+}
