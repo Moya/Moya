2398128c2ba2a6136fad2855628853c01f5a9bda
tesla <tesla@TanShengdeMacBook-Pro.local>
2017-09-13 15:14:31 +0800

    merge files

--- Sources/Moya/URL+Moya.swift
+++ Sources/Moya/URL+Moya.swift
@@ -14,3 +14,26 @@ public extension URL {
         }
     }
 }
+
+public extension String {
+    var md5: String {
+        if let data = base.data(using: .utf8, allowLossyConversion: true) {
+            
+            let message = data.withUnsafeBytes { bytes -> [UInt8] in
+                return Array(UnsafeBufferPointer(start: bytes, count: data.count))
+            }
+            
+            let MD5Calculator = MD5(message)
+            let MD5Data = MD5Calculator.calculate()
+            
+            var MD5String = String()
+            for c in MD5Data {
+                MD5String += String(format: "%02x", c)
+            }
+            return MD5String
+            
+        } else {
+            return base
+        }
+    }
+}
