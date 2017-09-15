62224866c6b72d442947b27de22c362f58de4f55
Łukasz Mróz <lukasz.mroz@droidsonroids.pl>
2017-09-15 19:57:39 +0200

    [NetworkActivityPlugin] Fix tests for `networkActivityClosure` changes

--- Tests/MoyaProviderSpec.swift
+++ Tests/MoyaProviderSpec.swift
@@ -112,9 +112,12 @@ class MoyaProviderSpec: QuickSpec {
 
         it("notifies at the beginning of network requests") {
             var called = false
-            let plugin = NetworkActivityPlugin { (change) -> Void in
+            var calledTarget: TargetType?
+
+            let plugin = NetworkActivityPlugin { change, target in
                 if change == .began {
                     called = true
+                    calledTarget = target
                 }
             }
 
@@ -123,13 +126,17 @@ class MoyaProviderSpec: QuickSpec {
             provider.request(target) { _ in  }
 
             expect(called) == true
+            expect(calledTarget).toNot(beNil())
         }
 
         it("notifies at the end of network requests") {
             var called = false
-            let plugin = NetworkActivityPlugin { (change) -> Void in
+            var calledTarget: TargetType?
+
+            let plugin = NetworkActivityPlugin { change, target in
                 if change == .ended {
                     called = true
+                    calledTarget = target
                 }
             }
 
@@ -138,6 +145,7 @@ class MoyaProviderSpec: QuickSpec {
             provider.request(target) { _ in  }
 
             expect(called) == true
+            expect(calledTarget).toNot(beNil())
         }
 
         describe("a provider with delayed stubs") {
