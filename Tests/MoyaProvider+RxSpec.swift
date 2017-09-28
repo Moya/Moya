b38bf513a13707f1d647315853f78e7c3bf6697c
Steven Deutsch <stevensdeutsch@yahoo.com>
2017-09-28 04:47:43 -0500

    Fix SwiftLint warnings

--- Tests/MoyaProvider+RxSpec.swift
+++ Tests/MoyaProvider+RxSpec.swift
@@ -176,7 +176,7 @@ final class MoyaProviderRxSpec: QuickSpec {
                         case let .next(element):
                             nextProgressValues.append(element.progress)
 
-                            if let _ = element.response { nextResponseCount += 1 }
+                            if element.response != nil { nextResponseCount += 1 }
                         case .error: errorEventsCount += 1
                         case .completed: completedEventsCount += 1
                         }
