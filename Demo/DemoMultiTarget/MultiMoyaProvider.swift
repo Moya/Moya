e07d34fd723dbd98c986fa7979ca261e855d0519
Andrey Yastrebov <ayastrebov@luxoft.com>
2017-09-28 22:29:17 +0300

    - Add MultiTarget sample

---
+++ Demo/DemoMultiTarget/MultiMoyaProvider.swift
@@ -0,0 +1,49 @@
+import Foundation
+import Moya
+import Result
+
+protocol TestResultType {
+    associatedtype T
+    static func parse(_ object: Any) -> Self?
+}
+
+protocol DecodableTargetType: Moya.TargetType {
+    associatedtype ResultType: TestResultType
+}
+
+final class MultiMoyaProvider: MoyaProvider<MultiTarget> {
+    
+    typealias Target = MultiTarget
+    
+    override init(endpointClosure: @escaping EndpointClosure = MoyaProvider.defaultEndpointMapping,
+                  requestClosure: @escaping RequestClosure = MoyaProvider.defaultRequestMapping,
+                  stubClosure: @escaping StubClosure = MoyaProvider.neverStub,
+                  callbackQueue: DispatchQueue? = nil,
+                  manager: Manager = MoyaProvider<Target>.defaultAlamofireManager(),
+                  plugins: [PluginType] = [],
+                  trackInflights: Bool = false) {
+        
+        super.init(endpointClosure: endpointClosure,
+                   requestClosure: requestClosure,
+                   stubClosure: stubClosure,
+                   callbackQueue: callbackQueue,
+                   manager: manager,
+                   plugins: plugins,
+                   trackInflights: trackInflights)
+    }
+    
+    func requestDecoded<T: DecodableTargetType>(_ target: T, completion: @escaping (_ result: Result<T.ResultType, Moya.MoyaError>) -> ()) -> Cancellable {
+        return request(MultiTarget(target)) { result in
+            switch result {
+            case .success(let response):
+                if let parsed = T.ResultType.parse(try! response.mapJSON()) {
+                    completion(.success(parsed))
+                } else {
+                    completion(.failure(.jsonMapping(response)))
+                }
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+}
--- Demo/DemoMultiTarget/ViewController.swift
+++ Demo/DemoMultiTarget/ViewController.swift
@@ -1,7 +1,7 @@
 import UIKit
 import Moya
 
-let provider = MoyaProvider<MultiTarget>(plugins: [NetworkLoggerPlugin(verbose: true)])
+let provider = MultiMoyaProvider(plugins: [NetworkLoggerPlugin(verbose: true)])
 
 class ViewController: UITableViewController {
     var progressView = UIView()
