0ed2eb1574c494197eb628524c41a50d96d3b7fc
James Sherlock <Sherlouk@users.noreply.github.com>
2017-09-28 18:25:43 +0100

    Attempt a build with valid changes to ensure still works

--- scripts/check-non-trivial-changes.sh
+++ scripts/check-non-trivial-changes.sh
@@ -2,8 +2,7 @@
 
 CHANGED_FILES=$(git show --name-only --pretty="" $CIRCLE_SHA1)
 
-# Temporary regex added .sh to test that it ignores the fact I've changed this file
-if grep -qvE '(\.(md|sh|yml)$)|(^(docs|web))/' <<< $CHANGED_FILES ; then
+if grep -qvE '(\.(md|yml)$)|(^(docs|web))/' <<< $CHANGED_FILES ; then
   exit 0
 fi
 
