5950abc2028a58cb66bf50a39f67af1e13f50ca1
James Sherlock <James.Sherlock@sky.uk>
2017-09-28 14:28:24 +0100

    Move non-trivial check to be as part of test pre-checks
    As per CircleCI's documentation each of the "sections" (test, checkout, dependencies) are all run independently so have little effect on each other. Struggling to find a nice way to stop execution of next steps without bombing out an error code!

--- scripts/check-non-trivial-changes.sh
+++ scripts/check-non-trivial-changes.sh
@@ -3,9 +3,9 @@
 CHANGED_FILES=$(git show --name-only --pretty="" $CIRCLE_SHA1)
 
 # Temporary regex added .sh to test that it ignores the fact I've changed this file
-if grep -qvE '(\.(md|sh)$)|(^(docs|web))/' <<< $CHANGED_FILES ; then
+if grep -qvE '(\.(md|sh|yml)$)|(^(docs|web))/' <<< $CHANGED_FILES ; then
   exit 0
 fi
 
 echo "Only docs were updated, stopping build!"
-circleci step halt
+exit 1
