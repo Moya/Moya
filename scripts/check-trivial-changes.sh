30f4a6d021c96a1bfdb74075d34544b1cb29839d
Pedro Vereza <pdrvereza@gmail.com>
2017-09-30 15:16:04 -0300

    WIP - just testing it out

--- scripts/check-trivial-changes.sh
+++ scripts/check-trivial-changes.sh
@@ -3,8 +3,8 @@
 CHANGED_FILES=$(git show --name-only --pretty="" $CIRCLE_SHA1)
 
 if grep -qvE '(\.(md|yml)$)|(^(docs|web))/' <<< $CHANGED_FILES ; then
-  exit 0
+  exit 1
 fi
 
 echo "Only docs were updated, stopping build!"
-exit 1
+exit 0
