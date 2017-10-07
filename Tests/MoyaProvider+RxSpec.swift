7caf510833be2ef158649a56281a3a5d71b47c7b
Afonso Graça <afonso.graca@me.com>
2017-10-07 02:03:12 +0100

    chore: update deprecated method from RxSwift

--- Tests/MoyaProvider+RxSpec.swift
+++ Tests/MoyaProvider+RxSpec.swift
@@ -223,7 +223,7 @@ final class MoyaProviderRxSpec: QuickSpec {
                                     .subscribe(onSuccess: { _ in
                                         callbackQueueLabel = DispatchQueue.currentLabel
                                         completion()
-                                    }).addDisposableTo(disposeBag)
+                                    }).disposed(by: disposeBag)
                             })
 
                             expect(callbackQueueLabel) == requestQueue.label
@@ -239,7 +239,7 @@ final class MoyaProviderRxSpec: QuickSpec {
                                     .subscribe(onSuccess: { _ in
                                         callbackQueueLabel = DispatchQueue.currentLabel
                                         completion()
-                                    }).addDisposableTo(disposeBag)
+                                    }).disposed(by: disposeBag)
                             })
 
                             expect(callbackQueueLabel) == callbackQueue.label
@@ -267,7 +267,7 @@ final class MoyaProviderRxSpec: QuickSpec {
                                     .subscribe(onSuccess: { _ in
                                         callbackQueueLabel = DispatchQueue.currentLabel
                                         completion()
-                                    }).addDisposableTo(disposeBag)
+                                    }).disposed(by: disposeBag)
                             })
 
                             expect(callbackQueueLabel) == requestQueue.label
@@ -283,7 +283,7 @@ final class MoyaProviderRxSpec: QuickSpec {
                                     .subscribe(onSuccess: { _ in
                                         callbackQueueLabel = DispatchQueue.currentLabel
                                         completion()
-                                    }).addDisposableTo(disposeBag)
+                                    }).disposed(by: disposeBag)
                             })
 
                             expect(callbackQueueLabel) == DispatchQueue.main.label
