fc789fcfc88f2ea9bde9d069101c76ed898ce712
Victor Sharavara <victor.sharavara@gmail.com>
2017-10-25 22:16:06 +0300

    Added missing content type for requestJSONEncodable task

--- Sources/Moya/URLRequest+Encoding.swift
+++ Sources/Moya/URLRequest+Encoding.swift
@@ -6,6 +6,11 @@ internal extension URLRequest {
         do {
             let encodable = AnyEncodable(encodable)
             httpBody = try JSONEncoder().encode(encodable)
+
+            if value(forHTTPHeaderField: "Content-Type") == nil {
+                setValue("application/json", forHTTPHeaderField: "Content-Type")
+            }
+
             return self
         } catch {
             throw MoyaError.encodableMapping(error)
