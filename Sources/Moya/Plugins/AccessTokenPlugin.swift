c85a9f95fe489acad95d0486dff7ba5d5da9fb98
Florian Fittschen <ffittschen@gmail.com>
2018-01-02 11:57:07 +0100

    Improve AccessTokenPlugin

--- Sources/Moya/Plugins/AccessTokenPlugin.swift
+++ Sources/Moya/Plugins/AccessTokenPlugin.swift
@@ -1,73 +1,95 @@
 import Foundation
 import Result
 
-// MARK: - AccessTokenAuthorizable
+/// HTTP authentication scheme
+///
+/// As declared in the [HTTP Authentication Scheme Registry](https://www.iana.org/assignments/http-authschemes/http-authschemes.xhtml)
+public enum AuthenticationScheme {
+    case basic
+    case bearer
 
-/// A protocol for controlling the behavior of `AccessTokenPlugin`.
-public protocol AccessTokenAuthorizable {
+    var headerField: String {
+        return "Authorization"
+    }
 
-    /// Represents the authorization header to use for requests.
-    var authorizationType: AuthorizationType { get }
+    var tokenPrefix: String {
+        switch self {
+        case .basic:
+            return "Basic"
+        case .bearer:
+            return "Bearer"
+        }
+    }
 }
 
-// MARK: - AuthorizationType
+public enum TokenPlacement {
+    case header
+    case queryParameter
+}
 
-/// An enum representing the header to use with an `AccessTokenPlugin`
-public enum AuthorizationType: String {
+public enum AccessControlType {
     case none
-    case basic = "Basic"
-    case bearer = "Bearer"
+    case http(scheme: AuthenticationScheme)
+    case apiKey(name: String, placement: TokenPlacement)
 }
 
-// MARK: - AccessTokenPlugin
+public protocol AccessControllable {
+    var accessControlType: AccessControlType { get }
+}
 
-/**
- A plugin for adding basic or bearer-type authorization headers to requests. Example:
-
- ```
- Authorization: Bearer <token>
- Authorization: Basic <token>
- ```
-
-*/
 public struct AccessTokenPlugin: PluginType {
 
-    /// A closure returning the access token to be applied in the header.
+    /// A closure returning the access token to be applied in the header or query parameter.
     public let tokenClosure: () -> String
 
-    /**
-     Initialize a new `AccessTokenPlugin`.
-
-     - parameters:
-       - tokenClosure: A closure returning the token to be applied in the pattern `Authorization: <AuthorizationType> <token>`
-    */
-    public init(tokenClosure: @escaping @autoclosure () -> String) {
+    /// Initialize a new `AccessTokenPlugin`.
+    ///
+    /// - Parameter tokenClosure: A closure returning the token to be applied in the pattern `Authorization: <AuthenticationScheme> <token>`
+    public init(tokenClosure: @escaping () -> String) {
         self.tokenClosure = tokenClosure
     }
 
-    /**
-     Prepare a request by adding an authorization header if necessary.
-
-     - parameters:
-       - request: The request to modify.
-       - target: The target of the request.
-     - returns: The modified `URLRequest`.
-    */
+    /// Prepare a request by adding an authorization header or query parameter if necessary.
+    ///
+    /// - Parameters:
+    ///   - request: The request to modify.
+    ///   - target: The target of the request.
+    /// - Returns: The modified `URLRequest`.
     public func prepare(_ request: URLRequest, target: TargetType) -> URLRequest {
-        guard let authorizable = target as? AccessTokenAuthorizable else { return request }
-
-        let authorizationType = authorizable.authorizationType
+        guard let accessControllable = target as? AccessControllable else { return request }
 
         var request = request
 
-        switch authorizationType {
-        case .basic, .bearer:
-            let authValue = authorizationType.rawValue + " " + tokenClosure()
-            request.addValue(authValue, forHTTPHeaderField: "Authorization")
-        case .none:
+        switch accessControllable.accessControlType {
+        case .http(let scheme):
+            let authValue = scheme.tokenPrefix + " " + tokenClosure()
+            request.addValue(authValue, forHTTPHeaderField: scheme.headerField)
+        case .apiKey(let name, let placement) where placement == .header:
+            request.addValue(tokenClosure(), forHTTPHeaderField: name)
+        case .apiKey(let name, let placement) where placement == .queryParameter:
+            guard let url = request.url else { return request }
+            request.url = addOrAppendQueryParameterToURL(url, toParameter: name)
+        default:
             break
         }
 
         return request
+    }
+
+    private func addOrAppendQueryParameterToURL(_ url: URL, toParameter name: String) -> URL {
+        guard var components = URLComponents(url: url, resolvingAgainstBaseURL: false) else { return url }
+
+        if components.queryItems != nil {
+            // swiftlint:disable:next force_unwrapping
+            components.queryItems!.append(URLQueryItem(name: name, value: tokenClosure()))
+        } else {
+            components.queryItems = [URLQueryItem(name: name, value: tokenClosure())]
+        }
+
+        do {
+            return try components.asURL()
+        } catch {
+            return url
+        }
     }
 }
--- Tests/AccessTokenPluginSpec.swift
+++ Tests/AccessTokenPluginSpec.swift
@@ -4,20 +4,35 @@ import Moya
 import Result
 
 final class AccessTokenPluginSpec: QuickSpec {
-    struct TestTarget: TargetType, AccessTokenAuthorizable {
-        let baseURL = URL(string: "http://www.api.com/")!
-        let path = ""
-        let method = Method.get
-        let task = Task.requestPlain
-        let sampleData = Data()
-        let headers: [String: String]? = nil
+    struct TestTarget: TargetType, AccessControllable {
+        let baseURL: URL
+        let path: String
+        let method: Moya.Method
+        let task: Task
+        let sampleData: Data
+        let headers: [String: String]?
+        let accessControlType: AccessControlType
 
-        let authorizationType: AuthorizationType
+        init(baseURL: URL = URL(string: "http://www.api.com/")!,
+             path: String = "",
+             method: Moya.Method = .get,
+             task: Task = .requestPlain,
+             sampleData: Data = Data(),
+             headers: [String: String]? = nil,
+             accessControlType: AccessControlType) {
+            self.baseURL = baseURL
+            self.path = path
+            self.method = method
+            self.task = task
+            self.sampleData = sampleData
+            self.headers = headers
+            self.accessControlType = accessControlType
+        }
     }
 
     override func spec() {
-        let token = "eyeAm.AJsoN.weBTOKen"
-        let plugin = AccessTokenPlugin(tokenClosure: token)
+        let tokenClosure = { return "eyeAm.AJsoN.weBTOKen" }
+        let plugin = AccessTokenPlugin(tokenClosure: tokenClosure)
 
         it("doesn't add an authorization header to TargetTypes by default") {
             let target = GitHub.zen
@@ -26,26 +41,68 @@ final class AccessTokenPluginSpec: QuickSpec {
             expect(preparedRequest.allHTTPHeaderFields).to(beNil())
         }
 
-        it("doesn't add an authorization header to AccessTokenAuthorizables when AuthorizationType is .none") {
-            let target = TestTarget(authorizationType: .none)
+        it("doesn't add query parameter authentication to TargetTypes by default") {
+            let target = GitHub.zen
+            let request = URLRequest(url: target.baseURL)
+            let preparedRequest = plugin.prepare(request, target: target)
+            let components = URLComponents(url: preparedRequest.url!, resolvingAgainstBaseURL: false)!
+            expect(components.queryItems).to(beNil())
+        }
+
+        it("doesn't add an authorization header to AccessControllables when AccessControlType is .none") {
+            let target = TestTarget(accessControlType: .none)
             let request = URLRequest(url: target.baseURL)
             let preparedRequest = plugin.prepare(request, target: target)
             expect(preparedRequest.allHTTPHeaderFields).to(beNil())
         }
 
-        it("adds a bearer authorization header to AccessTokenAuthorizables when AuthorizationType is .bearer") {
-            let target = TestTarget(authorizationType: .bearer)
+        it("doesn't add query parameter authentication to AccessControllables when AccessControlType is .none") {
+            let target = TestTarget(accessControlType: .none)
+            let request = URLRequest(url: target.baseURL)
+            let preparedRequest = plugin.prepare(request, target: target)
+            let components = URLComponents(url: preparedRequest.url!, resolvingAgainstBaseURL: false)!
+            expect(components.queryItems).to(beNil())
+        }
+
+        it("adds a bearer authorization header to AccessControllables when AccessControlType is .http and the scheme is .bearer") {
+            let target = TestTarget(accessControlType: .http(scheme: .bearer))
             let request = URLRequest(url: target.baseURL)
             let preparedRequest = plugin.prepare(request, target: target)
             expect(preparedRequest.allHTTPHeaderFields) == ["Authorization": "Bearer eyeAm.AJsoN.weBTOKen"]
         }
 
-        it("adds a basic authorization header to AccessTokenAuthorizables when AuthorizationType is .basic") {
-            let target = TestTarget(authorizationType: .basic)
+        it("adds a basic authorization header to AccessControllables when AccessControlType is .http and the scheme is .basic") {
+            let target = TestTarget(accessControlType: .http(scheme: .basic))
             let request = URLRequest(url: target.baseURL)
             let preparedRequest = plugin.prepare(request, target: target)
             expect(preparedRequest.allHTTPHeaderFields) == ["Authorization": "Basic eyeAm.AJsoN.weBTOKen"]
         }
 
+        it("adds a custom authorization header to AccessControllables when AccessControlType is .apiKey and the placement is .header") {
+            let target = TestTarget(accessControlType: .apiKey(name: "X-Access-Token", placement: .header))
+            let request = URLRequest(url: target.baseURL)
+            let preparedRequest = plugin.prepare(request, target: target)
+            expect(preparedRequest.allHTTPHeaderFields) == ["X-Access-Token": "eyeAm.AJsoN.weBTOKen"]
+        }
+
+        it("adds a query parameter to AccessControllables when AccessControlType is .apiKey and the placement is .queryParameter") {
+            let target = TestTarget(accessControlType: .apiKey(name: "accessToken", placement: .queryParameter))
+            let request = URLRequest(url: target.baseURL)
+            let preparedRequest = plugin.prepare(request, target: target)
+            let components = URLComponents(url: preparedRequest.url!, resolvingAgainstBaseURL: false)!
+            expect(components.queryItems) == [URLQueryItem(name: "accessToken", value: "eyeAm.AJsoN.weBTOKen")]
+        }
+
+        it("appends a query parameter to AccessControllables when AccessControlType is .apiKey and the placement is .queryParameter") {
+            let target = TestTarget(baseURL: URL(string: "http://www.api.com/?key=value")!,
+                                    accessControlType: .apiKey(name: "accessToken", placement: .queryParameter))
+            let request = URLRequest(url: target.baseURL)
+            let preparedRequest = plugin.prepare(request, target: target)
+            let components = URLComponents(url: preparedRequest.url!, resolvingAgainstBaseURL: false)!
+            expect(components.queryItems) == [
+                URLQueryItem(name: "key", value: "value"),
+                URLQueryItem(name: "accessToken", value: "eyeAm.AJsoN.weBTOKen")
+            ]
+        }
     }
 }
