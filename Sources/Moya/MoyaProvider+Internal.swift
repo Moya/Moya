1936801555f1f4a4667bcb14105805942d86d11e
Steven Deutsch <stevensdeutsch@yahoo.com>
2018-01-19 11:21:11 -0600

    Fix bad reference to target instead of endpoint

--- Sources/Moya/MoyaProvider+Internal.swift
+++ Sources/Moya/MoyaProvider+Internal.swift
@@ -90,13 +90,13 @@ public extension MoyaProvider {
     private func performRequest(_ target: Target, request: URLRequest, callbackQueue: DispatchQueue?, progress: Moya.ProgressBlock?, completion: @escaping Moya.Completion, endpoint: Endpoint<Target>, stubBehavior: Moya.StubBehavior) -> Cancellable {
         switch stubBehavior {
         case .never:
-            switch target.task {
+            switch endpoint.task {
             case .requestPlain, .requestData, .requestJSONEncodable, .requestParameters, .requestCompositeData, .requestCompositeParameters:
                 return self.sendRequest(target, request: request, callbackQueue: callbackQueue, progress: progress, completion: completion)
             case .uploadFile(let file):
                 return self.sendUploadFile(target, request: request, callbackQueue: callbackQueue, file: file, progress: progress, completion: completion)
             case .uploadMultipart(let multipartBody), .uploadCompositeMultipart(let multipartBody, _):
-                guard !multipartBody.isEmpty && target.method.supportsMultipart else {
+                guard !multipartBody.isEmpty && endpoint.method.supportsMultipart else {
                     fatalError("\(target) is not a multipart upload target.")
                 }
                 return self.sendUploadMultipart(target, request: request, callbackQueue: callbackQueue, multipartBody: multipartBody, progress: progress, completion: completion)
