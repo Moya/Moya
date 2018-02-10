b2307a89daec29ebdb96427497f1c29b00021c8d
Joan Disho <dishojoan@gmail.com>
2018-02-10 19:33:02 +0100

    Update Single+Response
    Map received data at key path into a Decodable object and if the conversion fails, the nil is returned instead of error signal.

--- Sources/RxMoya/Single+Response.swift
+++ Sources/RxMoya/Single+Response.swift
@@ -10,53 +10,67 @@ extension PrimitiveSequence where TraitType == SingleTrait, ElementType == Respo
     /// Filters out responses that don't fall within the given range, generating errors when others are encountered.
     public func filter(statusCodes: ClosedRange<Int>) -> Single<ElementType> {
         return flatMap { response -> Single<ElementType> in
-            return Single.just(try response.filter(statusCodes: statusCodes))
+            return .just(try response.filter(statusCodes: statusCodes))
         }
     }
 
     public func filter(statusCode: Int) -> Single<ElementType> {
         return flatMap { response -> Single<ElementType> in
-            return Single.just(try response.filter(statusCode: statusCode))
+            return .just(try response.filter(statusCode: statusCode))
         }
     }
 
     public func filterSuccessfulStatusCodes() -> Single<ElementType> {
         return flatMap { response -> Single<ElementType> in
-            return Single.just(try response.filterSuccessfulStatusCodes())
+            return .just(try response.filterSuccessfulStatusCodes())
         }
     }
 
     public func filterSuccessfulStatusAndRedirectCodes() -> Single<ElementType> {
         return flatMap { response -> Single<ElementType> in
-            return Single.just(try response.filterSuccessfulStatusAndRedirectCodes())
+            return .just(try response.filterSuccessfulStatusAndRedirectCodes())
         }
     }
 
     /// Maps data received from the signal into an Image. If the conversion fails, the signal errors.
     public func mapImage() -> Single<Image?> {
         return flatMap { response -> Single<Image?> in
-            return Single.just(try response.mapImage())
+            return .just(try response.mapImage())
         }
     }
 
     /// Maps data received from the signal into a JSON object. If the conversion fails, the signal errors.
     public func mapJSON(failsOnEmptyData: Bool = true) -> Single<Any> {
         return flatMap { response -> Single<Any> in
-            return Single.just(try response.mapJSON(failsOnEmptyData: failsOnEmptyData))
+            return .just(try response.mapJSON(failsOnEmptyData: failsOnEmptyData))
         }
     }
 
     /// Maps received data at key path into a String. If the conversion fails, the signal errors.
     public func mapString(atKeyPath keyPath: String? = nil) -> Single<String> {
         return flatMap { response -> Single<String> in
-            return Single.just(try response.mapString(atKeyPath: keyPath))
+            return .just(try response.mapString(atKeyPath: keyPath))
         }
     }
 
     /// Maps received data at key path into a Decodable object. If the conversion fails, the signal errors.
     public func map<D: Decodable>(_ type: D.Type, atKeyPath keyPath: String? = nil, using decoder: JSONDecoder = JSONDecoder(), failsOnEmptyData: Bool = true) -> Single<D> {
         return flatMap { response -> Single<D> in
-            return Single.just(try response.map(type, atKeyPath: keyPath, using: decoder, failsOnEmptyData: failsOnEmptyData))
+            return .just(try response.map(type, atKeyPath: keyPath, using: decoder, failsOnEmptyData: failsOnEmptyData))
         }
     }
+    
+    /// Maps received data at key path into a Decodable object.
+    /// If the conversion fails, the nil is returned instead of error signal.
+    public func mapOptional<D: Decodable>(_ type: D.Type, atKeyPath keyPath: String? = nil, using decoder: JSONDecoder = JSONDecoder()) -> Single<D?> {
+        return flatMap { response -> Single<D?> in
+            do {
+                let object = try response.map(type, atKeyPath: keyPath, using: decoder)
+                return .just(object)
+            } catch {
+                return .just(nil)
+            }   
+        }
+    }
+
 }
