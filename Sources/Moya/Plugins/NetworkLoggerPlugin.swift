3c8fec1de0d39b98a0ee06500737288986186967
Alan Yeo <me@alanyjw.com>
2018-04-18 15:52:00 +0800

    Break up initializer arguments to be multi-lined

--- Sources/Moya/Plugins/NetworkLoggerPlugin.swift
+++ Sources/Moya/Plugins/NetworkLoggerPlugin.swift
@@ -18,7 +18,11 @@ public final class NetworkLoggerPlugin: PluginType {
     public let cURL: Bool
 
     /// Initializes a NetworkLoggerPlugin.
-    public init(verbose: Bool = false, cURL: Bool = false, output: ((_ separator: String, _ terminator: String, _ items: Any...) -> Void)? = nil, requestDataFormatter: ((Data) -> (String))? = nil, responseDataFormatter: ((Data) -> (Data))? = nil) {
+    public init(verbose: Bool = false,
+                cURL: Bool = false,
+                output: ((_ separator: String, _ terminator: String, _ items: Any...) -> Void)? = nil,
+                requestDataFormatter: ((Data) -> (String))? = nil,
+                responseDataFormatter: ((Data) -> (Data))? = nil) {
         self.cURL = cURL
         self.isVerbose = verbose
         self.output = output ?? NetworkLoggerPlugin.reversedPrint
