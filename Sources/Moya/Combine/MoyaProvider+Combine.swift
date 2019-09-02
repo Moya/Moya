99397c22e24d5f09a65d77e600623eacde460d14
Łukasz Mróz <thesunshinejr@gmail.com>
2019-09-02 17:54:04 +0200

    Implement requestWithProgress for Combine extensions

--- Sources/Moya/Combine/MoyaProvider+Combine.swift
+++ Sources/Moya/Combine/MoyaProvider+Combine.swift
@@ -26,6 +26,37 @@ public extension Publishable where Base: MoyaProviderType {
             }
             .eraseToAnyPublisher()
     }
+
+    /// Designated request-making method with progress.
+    func requestWithProgress(_ target: Base.Target, callbackQueue: DispatchQueue? = nil) -> AnyPublisher<ProgressResponse, MoyaError> {
+        let progressBlock: (AnySubscriber<ProgressResponse, MoyaError>) -> (ProgressResponse) -> Void = { subscriber in
+            return { progress in
+                _ = subscriber.receive(progress)
+            }
+        }
+
+        let response = MoyaPublisher<ProgressResponse> { [weak base] subscriber in
+            let cancellableToken = base?.request(target, callbackQueue: callbackQueue, progress: progressBlock(subscriber)) { result in
+                switch result {
+                case .success:
+                    subscriber.receive(completion: .finished)
+                case let .failure(error):
+                    subscriber.receive(completion: .failure(error))
+                }
+            }
+
+            return cancellableToken
+        }
+
+        // Accumulate all progress and combine them when the result comes
+        return response
+            .scan(ProgressResponse()) { last, progress in
+                let progressObject = progress.progressObject ?? last.progressObject
+                let response = progress.response ?? last.response
+                return ProgressResponse(progress: progressObject, response: response)
+            }
+            .eraseToAnyPublisher()
+    }
 }
 
 #endif
