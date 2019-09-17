bae4d5b366492b470136090af8727d88a98add84
Łukasz Mróz <thesunshinejr@gmail.com>
2019-09-17 11:57:45 +0200

    Update the script for carthage-xcode10-compat

--- scripts/carthage-xcode10-compat.sh
+++ scripts/carthage-xcode10-compat.sh
@@ -1,10 +1,10 @@
-# This code is replacing `-weak_framework Combine` with `` for Xcode 10 builds and the other way around for Xcode 11+.
-# Can be removed once we deprecate support for Xcode 10? Not sure when it's gonna happen though.
+#!/bin/bash
+
+# This code is replacing `-weak_framework Combine` with `` for Xcode < 11 builds.
+# Can be removed once we stop supporting Xcode 9/10.
 # When removing this script, also:
 #     - remove two user-defined build settings: OTHER_LDFLAGS_XCODE10, OTHER_LDFLAGS_XCODE11
 #     - also remove value for OTHER_LDFLAGS build settings (which is probably gonna equal $OTHER_LDFLAGS_XCODE11)
-if [ $XCODE_VERSION_MAJOR -ge 1100 ]; then
-    sed -ie 's,OTHER_LDFLAGS = $OTHER_LDFLAGS_XCODE10,OTHER_LDFLAGS = $OTHER_LDFLAGS_XCODE11,g' "$SRCROOT/Moya.xcodeproj/project.pbxproj"
-else
-    sed -ie 's,OTHER_LDFLAGS = $OTHER_LDFLAGS_XCODE11,OTHER_LDFLAGS = $OTHER_LDFLAGS_XCODE10,g' "$SRCROOT/Moya.xcodeproj/project.pbxproj"
-fi;
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+sed -ie 's,OTHER_LDFLAGS = $OTHER_LDFLAGS_XCODE11,OTHER_LDFLAGS = $OTHER_LDFLAGS_XCODE10,g' "$DIR/../Moya.xcodeproj/project.pbxproj"
