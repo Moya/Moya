1f5c00ac068362b38809613c5f14e27bc2e56060
Amaury DAVID <amaury@smartnsoft.com>
2019-10-30 11:25:51 +0100

    Added some doc.

--- Sources/Moya/AnyEncodable.swift
+++ Sources/Moya/AnyEncodable.swift
@@ -1,5 +1,7 @@
 import Foundation
 
+/// A simple type wrapping an Encodable to provide a workaround for errors
+/// of kind `Protocol type 'Encodable' cannot conform to 'Encodable' because only concrete types can conform to protocols`
 public struct AnyEncodable {
 
     private let encodable: Encodable
@@ -15,8 +17,8 @@ extension AnyEncodable: Encodable {
     }
 }
 
-extension AnyEncodable {
-    public var underlyingEncodable: Encodable {
+public extension AnyEncodable {
+    var underlyingEncodable: Encodable {
         if let embeddedAnyEncodable = encodable as? AnyEncodable {
             return embeddedAnyEncodable.underlyingEncodable
         }
--- Sources/Moya/Task.swift
+++ Sources/Moya/Task.swift
@@ -3,20 +3,43 @@ import Foundation
 /// Represents an HTTP task.
 public enum Task {
 
+    /// All different ways to set parameters in an HTTP request's body.
     public enum BodyParams {
+        /// The given encodable will be set in the request's body without any additional encoding.
         case raw(Data)
+
+        /// The given encodable will be json encoded in the request's body.
         case json(Encodable, JSONParameterEncoder = .default)
+
+        /// The given encodable will be url encoded in the request's body.
+        ///
+        /// The provided encoder must use the `.httpBody` destination or a `MoyaError.encodableMapping` will be raised.
         case urlEncoded(Encodable, URLEncodedFormParameterEncoder = .init(destination: .httpBody))
+
+        /// The given encodable will be encoded in the request's body using the provided encoder.
+        ///
+        /// The provided encoder must not be a `URLEncodedFormParameterEncoder` (use `BodyParams.urlEncoded`instead)
+        /// or `JSONParameterEncoder`(use `BodyParams.json` instead). If this is the case, a `MoyaError.encodableMapping` will be raised.
         case custom(Encodable, ParameterEncoder)
     }
 
+    /// All different ways to set parameters in an HTTP request's query.
     public enum QueryParams {
+        /// The given encodable will be url encoded in the request's query.
+        ///
+        /// The provided encoder must use the `.queryString` destination or a `MoyaError.encodableMapping` will be raised.
         case query(Encodable, URLEncodedFormParameterEncoder = .init(destination: .queryString))
     }
 
+    /// All sources available for use when uploading
     public enum UploadSource {
+        /// Upload the provided data
         case rawData(Data)
+
+        /// Upload the file at the given url
         case file(URL)
+
+        /// Upload some multipart content.
         case multipart([MultipartFormData])
     }
 
