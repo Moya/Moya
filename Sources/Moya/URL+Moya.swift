9f9b2c54c9e7495c1a5d64fa0cca8d81bc0e4365
dariukas <dariui.miliauskui@gmail.com>
2019-11-08 19:47:13 +0200

    Update URL+Moya Change
    Currently, without I put target.path into targetPath, in most cases target.path is called twice (when I setting path using Moya in a specific project) which makes an additional call: difficulties to debug, and performance change even small.

--- Sources/Moya/URL+Moya.swift
+++ Sources/Moya/URL+Moya.swift
@@ -7,10 +7,12 @@ public extension URL {
         // When a TargetType's path is empty, URL.appendingPathComponent may introduce trailing /, which may not be wanted in some cases
         // See: https://github.com/Moya/Moya/pull/1053
         // And: https://github.com/Moya/Moya/issues/1049
-        if target.path.isEmpty {
+        let targetPath = target.path
+        let targetBaseURL = target.baseURL
+        if targetPath.isEmpty {
             self = target.baseURL
         } else {
-            self = target.baseURL.appendingPathComponent(target.path)
+            self = target.baseURL.appendingPathComponent(targetPath)
         }
     }
 }
