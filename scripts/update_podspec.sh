b277a528a36ea8858ac5c760812ee36f82a5119e
Franco Meloni <franco.meloni91@gmail.com>
2019-12-29 21:45:28 +0000

    Added script to generate podspec

---
+++ scripts/update_podspec.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+SEMVER_REGEX="s\.version      = \"[0-9]*\.[0-9]*\.[0-9]*\(-.*\)\{0,1\}\""
+sed "s/$SEMVER_REGEX/s.version      = \"$VERSION\"/" Moya.podspec > tmpPodspec
+mv -f tmpPodspec Moya.podspec
