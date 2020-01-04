f9fffa2c2dd0ae120bbf4021856be194fa5de1b2
Franco Meloni <franco.meloni91@gmail.com>
2020-01-04 17:08:45 +0000

    Make rocket a dev dependency

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
         .package(url: "https://github.com/Quick/Quick.git", .upToNextMajor(from: "2.0.0")), // dev
         .package(url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "8.0.0")), // dev
         .package(url: "https://github.com/AliSoftware/OHHTTPStubs.git", .branch("feature/spm-support")), // dev
-        .package(url: "https://github.com/shibapm/Rocket", .upToNextMajor(from: "1.0.0")),
+        .package(url: "https://github.com/shibapm/Rocket", .upToNextMajor(from: "1.0.0")), // dev
     ],
     targets: [
         .target(name: "Moya", dependencies: ["Alamofire"]),
