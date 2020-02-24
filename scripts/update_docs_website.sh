52740dc9a59d76e8355c526873c10f98d2998011
Łukasz Mróz <thesunshinejr@gmail.com>
2020-02-24 12:34:23 +0100

    Add "docs/" folder (Guides) to the the generated moya website

--- scripts/update_docs_website.sh
+++ scripts/update_docs_website.sh
@@ -4,7 +4,7 @@ git clone git@github.com:Moya/moya.github.io.git _moya.github.io
 # Otherwise we would sometimes get empty docs due to some cache bug & new Xcode file response
 # Relevant: https://github.com/realm/jazzy/issues/1087
 rm -rf ~/Library/Developer/Xcode/DerivedData/Moya*
-jazzy -x USE_SWIFT_RESPONSE_FILE=NO -o _moya.github.io
+jazzy --documentation=docs/*.md -x USE_SWIFT_RESPONSE_FILE=NO -o _moya.github.io
 
 cd _moya.github.io
 
