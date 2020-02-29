ab031afffb8f09146da04173b9461a3ed141b478
Joan Disho <dishojoan@gmail.com>
2020-02-29 21:31:55 +0100

    Create Path
    Path is a combination of an endpoint and a method.

---
+++ Sources/Moya/Path.swift
@@ -0,0 +1,51 @@
+import Foundation
+
+public enum Path {
+    case connect(endpoint: String)
+    case delete(endpoint: String)
+    case get(endpoint: String)
+    case head(endpoint: String)
+    case options(endpoint: String)
+    case patch(endpoint: String)
+    case post(endpoint: String)
+    case put(endpoint: String)
+    case trace(endpoint: String)
+
+    public var endpoint: String {
+        switch self {
+        case let .connect(endpoint),
+             let .delete(endpoint),
+             let .get(endpoint),
+             let .head(endpoint),
+             let .options(endpoint),
+             let .patch(endpoint),
+             let .post(endpoint),
+             let .put(endpoint),
+             let .trace(endpoint):
+            return endpoint
+        }
+    }
+
+    public var method: Method {
+        switch self {
+        case .connect:
+            return .connect
+        case .delete:
+            return .delete
+        case .get:
+            return .get
+        case .head:
+            return .head
+        case .options:
+            return .options
+        case .patch:
+            return .patch
+        case .post:
+            return .post
+        case .put:
+            return .put
+        case .trace:
+            return .trace
+        }
+    }
+}
