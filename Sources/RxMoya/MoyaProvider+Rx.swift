634b5b85f23eeea9aa43bc547a419bf22dc70961
Panha Uy <6673987+uypanha@users.noreply.github.com>
2021-05-03 21:33:35 +0700

    Update MoyaProvider+Rx.swift

--- Sources/RxMoya/MoyaProvider+Rx.swift
+++ Sources/RxMoya/MoyaProvider+Rx.swift
@@ -21,7 +21,7 @@ public extension Reactive where Base: MoyaProviderType {
                 case let .success(response):
                     single(.success(response))
                 case let .failure(error):
-                    single(.error(error))
+                    single(.failure(error))
                 }
             }
 
