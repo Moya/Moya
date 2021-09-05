1b9187b9f73a742690827e028df7792ada72c085
Andrew Breckenridge <asbreckenridge@me.com>
2021-09-05 04:13:43 -0700

    [gardening] remove unecessary headers and plists causing SPM warnings

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let rocketIfNeeded: [Package.Dependency]
 
-#if os(OSX) || os(Linux)
+#if os(macOS) || os(Linux)
 rocketIfNeeded = [
     .package(url: "https://github.com/shibapm/Rocket", .upToNextMajor(from: "1.2.0")) // dev
 ]
@@ -27,19 +27,19 @@ let package = Package(
         .library(name: "RxMoya", targets: ["RxMoya"])
     ],
     dependencies: [
-        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.4.3")),
-        .package(url: "https://github.com/ReactiveCocoa/ReactiveSwift.git", .upToNextMajor(from: "6.6.1")),
-        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.2.0")),
-        .package(url: "https://github.com/Quick/Quick.git", .upToNextMajor(from: "4.0.0")), // dev
-        .package(url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "9.2.0")), // dev
-        .package(url: "https://github.com/AliSoftware/OHHTTPStubs.git", .upToNextMajor(from: "9.0.0")) // dev
+        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.4.3"),
+        .package(url: "https://github.com/ReactiveCocoa/ReactiveSwift.git", from: "6.6.1"),
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "6.2.0"),
+        .package(url: "https://github.com/Quick/Quick.git", from: "4.0.0"), // dev
+        .package(url: "https://github.com/Quick/Nimble.git", from: "9.2.0"), // dev
+        .package(url: "https://github.com/AliSoftware/OHHTTPStubs.git", from: "9.0.0") // dev
     ] + rocketIfNeeded,
     targets: [
         .target(
             name: "Moya",
-            dependencies: [
-                .product(name: "Alamofire", package: "Alamofire")
-            ]
+          dependencies: [
+            .product(name: "Alamofire", package: "Alamofire")
+          ]
         ),
         .target(
             name: "CombineMoya",
